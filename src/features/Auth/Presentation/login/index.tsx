a4449333841b7636b15573f0cf91540acf3a66ad
Vuxuanthanh04 <vxt233@gmail.com>
2026-04-17 20:32:29 +0700

    viet dang nhap, dang ki

--- src/features/Auth/Presentation/login/index.tsx
+++ src/features/Auth/Presentation/login/index.tsx
@@ -1,9 +1,11 @@
 import { Form, Input, Button, Card } from "antd";
 import React from "react";
+import { Link } from "react-router-dom";
 import { FirebaseAuthRepositoryImpl } from "../../data/repositories/FirebaseAuthRepositoryImpl";
 import type { AuthRepository } from "../../domain/respositories/authRepository";
 
 export default class LoginPage extends React.Component {
+
     private authRepository: AuthRepository = new FirebaseAuthRepositoryImpl();
 
     onSubmit = async (values: { email: string, password: string }) => {
@@ -23,7 +25,7 @@ export default class LoginPage extends React.Component {
                     display: "flex",
                     justifyContent: "center",
                     alignItems: "center",
-                    backgroundImage: "url('/cac-mau-background-4k-cuc-sac-net-1.png')",
+                    backgroundImage: "url('/backgroundLogin.jpg')",
                     backgroundSize: "cover",
                     backgroundPosition: "center",
                     backgroundRepeat: "no-repeat",
@@ -44,12 +46,18 @@ export default class LoginPage extends React.Component {
                             <Input type="password" />
                         </Form.Item>
                         <Form.Item>
-                            <Button type="primary" htmlType="submit" block>
-                                Đăng Nhập
-                            </Button>
-                            <Button danger htmlType="submit" block>
-                                Đăng Kí
-                            </Button>
+                            <div style={{ display: "flex", gap: "10px" }}>
+                                <Link to="/" style={{ width: "50%", display: "block" }}>
+                                    <Button type="primary" block>
+                                        Đăng Nhập
+                                    </Button>
+                                </Link>
+                                <Button type="primary" danger style={{ flex: 1 }}>
+                                    <Link to="/register" style={{ color: "inherit", width: "100%", display: "block" }}>
+                                        Đăng Kí
+                                    </Link>
+                                </Button>
+                            </div>
                         </Form.Item>
                     </Form>
                 </Card>
